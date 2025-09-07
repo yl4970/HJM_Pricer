18aa36cca9fbb4c4f08a08c63c6c3da70ef86125
yeerobyte <75462898+yl4970@users.noreply.github.com>
2025-09-07 17:30:48 -0400

    Update volSurface.py

--- MC_simulation/volSurface.py
+++ MC_simulation/volSurface.py
@@ -1,78 +1,120 @@
 import numpy as np
 import pandas as pd
-from datetime import datetime as dt
-from typing import List, Dict
-from functools import cached_property
-from collections import namedtuple
-from MC_simulation.volatility import Volatility as vol, FitResult as FR
+from typing import Optional, Any, Callable, ParamSpec, Concatenate, Iterable
+from functools import wraps
+from dataclasses import dataclass, field
+from tqdm import tqdm
 
+from alt_volSurface.volatility import PCAResult
+
+DateKey = pd.Timestamp
+P = ParamSpec("P")
+
+def vectorize_over_dates(func:Callable[Concatenate[object, DateKey, P], tuple]):
+    @wraps(func)
+    def wrapper(self, dates:Iterable[object], **kwargs:P.kwargs):
+        sample_output = func(self, dates[0], **kwargs)
+        n_keys = 1
+        if isinstance(sample_output, dict):
+            keys = sample_output.keys()
+            n_keys = len(keys)
+        output_dict = [dict() for _ in range(n_keys)]
+
+        for date in tqdm(dates):
+            output = func(self, date, **kwargs)
+            if isinstance(output, dict):
+                for dct, out in zip(output_dict, output):
+                    dct[date] = output[out]
+            else:
+                output_dict[0][date] = output
+    
+        return output_dict[0] if n_keys==1 else tuple(output_dict)
+    return wrapper
+
+@dataclass(slots=True)
 class VolatilitySurface:
-    def __init__(self, 
-                 forward_curves:pd.DataFrame=None, 
-                 n_factors:int=3, 
-                 window_months:int=3):
-        
-        self.forward_curves_df = forward_curves
-        self.stationary_fwds_df = self.forward_curves_df.diff().dropna()
-        self.tenors = list(self.stationary_fwds_df.columns)
-        self.n_factors = n_factors
-        self.window_months = window_months    
+    forward_curves: pd.DataFrame = None
+    localVol_window_months: int = None
+    n_factors: int = None
 
+    _full_timeline: Optional[list[DateKey]] = field(default=None, init=False)
+    tenors: Optional[list[int]] = field(default=None, init=False)
+    timeline: Optional[list[DateKey]] = field(default=None, init=False)
+    bdays_dict: Optional[dict[int, int]] = field(default=None, init=False)
+    windowed_bdays: Optional[dict[DateKey, int]] = field(default=None, init=False)
+    windowed_fwds: Optional[dict[DateKey, pd.DataFrame]] = field(default=None, init=False)
+    windowed_fwds_df: pd.DataFrame = field(default=None, init=False)
+    localVols: Optional[dict[DateKey, PCAResult]] = field(default=None, init=False)
+
+    @property
+    def window(self) -> int:
+        return self.localVol_window_months
+
+    def __post_init__(self):
+        self._full_timeline = self.forward_curves.index
+        self.tenors = list(self.forward_curves.columns)
+        self.windowed_bdays = self._get_bdays_within_window(self._full_timeline)
         self._get_windowed_timeline()
-        self._get_localVols()
+        self.windowed_fwds_df = self.forward_curves.loc[self.timeline]
+        self._get_bdays_dict()
+        self._check_if_nobs_deficient()
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}(tenors={len(self.tenors)}, curve_start={self.timeline[0].strftime('%Y-%m-%d')}, curve_end={self.timeline[-1].strftime('%Y-%m-%d')}, rolling_window={self.window_months}mon, pca_n_components={self.n_factors}.)" 
-    
+    def build(self):
+        self.windowed_fwds = self._get_fwds_within_window(self.timeline)
+        self.localVols = self._pca(self.timeline)
+
     def _get_windowed_timeline(self):
-        # Make sure window is able to fit in the dataset timeline length
-        try: 
-            self.pd_Btimeline = pd.bdate_range(
-                start=self.forward_curves_df.index[0], 
-                end=self.forward_curves_df.index[-1] - pd.DateOffset(months=self.window_months)
-                ).tolist()
-            self.timeline = [i for i in self.pd_Btimeline if i in self.forward_curves_df.index]
-            self.windowed_forward_curves_df = self.forward_curves_df.loc[self.timeline]
-            
+        timeline_start = min(self.windowed_bdays.keys())
+        windowed_timeline_start = timeline_start + pd.offsets.BusinessDay(n=self.windowed_bdays[timeline_start])
+        self.timeline = [i for i in self._full_timeline if i>= windowed_timeline_start]
+
+    def _check_if_nobs_deficient(self):
+        n_obs = len(self.timeline)
+        min_windowed_bdays = min(self.windowed_bdays.items())[1]
+        if min_windowed_bdays > n_obs:
+            raise ValueError(f'Rolling window ({min_windowed_bdays}) exceeds available history ({n_obs}).')
+
+    # @vectorize_over_dates
+    # def _get_localVol(self, DateKey, windowed_fwds) -> pd.DataFrame:
+    #     return Volatility(windowed_fwds[DateKey], self.n_factors)
     
-        except: raise ValueError("Window size exceeds the time range of the provided DataFrame of forward curves.")
-
-    def _get_localVols(self) -> Dict[pd.Timestamp, vol]:
-        self.localVols= {}
-        for t in self.timeline:
-            windowed_bdays = self._get_BDays_in_window(t, self.window_months)
-            windowed_df = self._get_fwds_in_window(self.stationary_fwds_df, t, windowed_bdays)
-            windowed_cov_matrix = self._get_annualized_cov(windowed_df)
-            self.localVols[t] = vol(self.tenors, windowed_cov_matrix, self.n_factors)
-        return self.localVols
+    @vectorize_over_dates
+    def _get_fwds_within_window(self, DateKey) -> pd.DataFrame:
+        start = DateKey + pd.DateOffset(months=self.window * (-1))
+        if start + pd.tseries.offsets.BDay(0) != start:
+            start += pd.tseries.offsets.BDay(-1)
+        return self.forward_curves.loc[start : DateKey]
     
-    def polyfit(self, 
-                degrees:List[int]=None
-                ) -> Dict[pd.Timestamp, FR]:
-        self.fittedLocalVols = {}
-        for t in self.timeline:
-            self.fittedLocalVols[t] = self.localVols[t].polyfit(degrees)
-        return self.fittedLocalVols
+    @vectorize_over_dates
+    def _get_bdays_within_window(self, DateKey) -> int:
+        return len(pd.date_range(DateKey+pd.DateOffset(months=self.window * (-1)), DateKey, freq='B'))
+    
+    @vectorize_over_dates
+    def polyfit(self, DateKey, degrees:tuple[int]=None) -> dict:
+        return self.localVols[DateKey].polyfit(degrees)['fittedVols']
 
-    @staticmethod
-    def _get_BDays_in_window(
-        t:pd.Timestamp=None, 
-        window_months:int=3
-        ) -> int:
-        return len(pd.date_range(t, t+pd.DateOffset(months=window_months), freq='B'))
+    def _get_bdays_dict(self):
+        self.bdays_dict = {
+            year: len(
+                pd.bdate_range(start=f'{year}-1-1', end=f'{year}-12-31')) for year in 
+            list(range(self.timeline[0].year, self.timeline[-1].year+1))
+        }
 
-    @staticmethod
-    def _get_fwds_in_window(
-        forward_curves_df:pd.DataFrame=None, 
-        t:pd.Timestamp=None, 
-        num_BDays:int=66) -> pd.DataFrame:
-        return forward_curves_df.loc[t:t+pd.tseries.offsets.BDay(num_BDays)]
+    @vectorize_over_dates
+    def _pca(self, DateKey):
+        df = self.windowed_fwds[DateKey]
+        dX = np.diff(df, axis=0)
+        dX -= dX.mean(axis=0, keepdims=True)
+        m, n = dX.shape
+        if m < n:
+            M = 1/(m-1) * (dX @ dX.T)
+            s, U = np.linalg.eigh(M.astype(float))
+            idx = s.argsort()[::-1][:self.n_factors]
+            s, U = s[idx], U[:, idx]
 
-    @staticmethod
-    def _get_annualized_cov(
-        stationary_fwds_df:pd.DataFrame=None
-        ) -> np.ndarray:
-        year = stationary_fwds_df.index[0].year
-        bdays_in_year = len(pd.date_range(start=dt(year,1,1), end=dt(year,12,31)))
-        return np.cov(stationary_fwds_df.T) * bdays_in_year
-  
+            eps = 1e-18
+            V = (dX.T @ U) / np.sqrt((m-1) * np.maximum(s, eps))
+        else:
+            s, V = np.linalg.eigh(np.cov(dX))
+
+        return PCAResult(V=V, s=s, bdays_in_year=self.bdays_dict[DateKey.year], tenors=self.tenors)
